4cb8ae604b653a2ca91e4466add4d27f48b8b5bf
Abhi1997 <41516895+Abhi1997@users.noreply.github.com>
2025-05-10 11:46:52 +0545

    admin side editor and autor role user creation  addition

--- NepNewsBackend/routes/userRoutes.js
+++ NepNewsBackend/routes/userRoutes.js
@@ -5,12 +5,20 @@ const auth = require("../middlewares/authMiddleware");
 const {
   getAllUsers,
   updateUserRole,
+  deleteUser,
+  createUser,
 } = require("../controllers/userController");
 
-// Get all users (admin only)
+// Get all users (Admin only)
 router.get("/", auth, getAllUsers);
 
-// Update user role (admin only)
+// Update user role (Admin only)
 router.put("/:userId/role", auth, updateUserRole);
+
+// Delete a user (Admin only)
+router.delete("/:userId", auth, deleteUser);
+
+// Create a new user (Admin only)
+router.post("/", auth, createUser);
 
 module.exports = router;
